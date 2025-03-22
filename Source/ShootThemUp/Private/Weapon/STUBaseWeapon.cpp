62f974d27964ad646c85fbd310b6c9519244a97b
Ruslan Rashevskiy <bear27098@gmail.com>
2025-03-22 17:56:10 +0300

    Added Line Traces

--- Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -3,6 +3,11 @@
 
 #include "Weapon/STUBaseWeapon.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/Character.h"
+
 
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseWeapon, All, All)
@@ -20,11 +25,87 @@ ASTUBaseWeapon::ASTUBaseWeapon()
 void ASTUBaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
+
+	check(WeaponMesh);
 	
 }
 
+void ASTUBaseWeapon::MakeShot() {
+    if (!GetWorld()) {
+        return;
+	}
+
+
+    FVector TraceStart, TraceEnd;
+
+    if (!GetTraceData(TraceStart, TraceEnd)) return;
+	
+	FHitResult HitResult;
+    MakeHit(HitResult, TraceStart, TraceEnd);
+
+	if (HitResult.bBlockingHit) {
+        DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
+        DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
+
+		UE_LOG(LogBaseWeapon, Display, TEXT("Bone: %s"), *HitResult.BoneName.ToString());
+    } else {
+        DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
+	}
+}
+
 void ASTUBaseWeapon::Fire() {
-    UE_LOG(LogBaseWeapon, Display, TEXT("Fire!"));
+
+	MakeShot();
+}
+
+APlayerController* ASTUBaseWeapon::GetPlayerController() const {
+    const auto Player = Cast<ACharacter>(GetOwner());
+
+    if (!Player) {
+        return nullptr;
+    }
+
+    return Player->GetController<APlayerController>();
+}
+
+bool ASTUBaseWeapon::GetPlayerViewPoint(FVector &ViewLocation, FRotator &ViewRotaion) const {
+    const auto Controller = GetPlayerController();
+
+    if (!Controller) {
+        return false;
+    }
+
+    Controller->GetPlayerViewPoint(ViewLocation, ViewRotaion);
+    return true;
 }
 
 
+FVector ASTUBaseWeapon::GetMuzzleWorldLocation() const {
+
+    return WeaponMesh->GetSocketLocation(MuzzleSocketName);
+
+}
+
+
+bool ASTUBaseWeapon::GetTraceData(FVector &TraceStart, FVector &TraceEnd) const {
+
+    FVector ViewLocation;
+    FRotator ViewRotation;
+    if (!GetPlayerViewPoint(ViewLocation, ViewRotation)) return false;
+
+    TraceStart = ViewLocation;
+    const FVector ShootDirection = ViewRotation.Vector();
+    TraceEnd = TraceStart + ShootDirection * TraceMaxDistance;
+
+    return true;
+}
+
+void ASTUBaseWeapon::MakeHit(FHitResult &HitResult, const FVector &TraceStart, const FVector &TraceEnd) {
+    if (!GetWorld())
+        return;
+
+    FCollisionQueryParams CollisionParams;
+    CollisionParams.AddIgnoredActor(GetOwner());
+
+    GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECollisionChannel::ECC_Visibility, CollisionParams);
+}
--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -23,6 +23,25 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
   USkeletalMeshComponent *WeaponMesh;
 	// Called when the game starts or when spawned
+
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+  FName MuzzleSocketName = "MuzzleSocket";
+
+  UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+  float TraceMaxDistance = 1500.0f;
+
 	virtual void BeginPlay() override;
+
+	void MakeShot();
+
+	APlayerController *GetPlayerController() const;
+
+	bool GetPlayerViewPoint(FVector &ViewLocation, FRotator &ViewRotaion) const;
+
+	FVector GetMuzzleWorldLocation() const;
+
+	bool GetTraceData(FVector &TraceStart, FVector &TraceEnd) const;
+
+	void MakeHit(FHitResult &HitResult, const FVector &TraceStart, const FVector &TraceEnd);
 
 };
