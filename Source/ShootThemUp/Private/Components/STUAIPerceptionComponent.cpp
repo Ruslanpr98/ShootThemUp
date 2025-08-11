5cf8efaacfe3a42e22ac397af48df6cba9dadb57
Ruslan Rashevskiy <bear27098@gmail.com>
2025-08-11 17:06:52 +0300

    Added Team Split, Player dont kill each other if on the same Team

--- Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUAIPerceptionComponent.cpp
@@ -34,7 +34,10 @@ AActor *USTUAIPerceptionComponent::GetClosestEnemy() const {
     for (const auto PerceiveActor : PerceiveActors) {
         const auto HealthComponent = STUUtils::GetSTUPlayerComponent<USTUHealthComponent>(PerceiveActor);
 
-        if (HealthComponent && !HealthComponent->isDead()) { // TODO: check if enemies or not 
+        const auto PerceivePawn = Cast<APawn>(PerceiveActor);
+
+        const auto AreEnemies = PerceivePawn && STUUtils::AreEnemies(Controller, PerceivePawn->Controller);
+        if (HealthComponent && !HealthComponent->isDead() && AreEnemies) {
             const auto CurrentDistance = (PerceiveActor->GetActorLocation() - Pawn->GetActorLocation()).Size();
 
             if (CurrentDistance < BestDistance) {
